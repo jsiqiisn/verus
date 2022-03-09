7650038afd9a5ef5e37c6395789bdc5ee324b546
jsiqiisn <100444720+jsiqiisn@users.noreply.github.com>
2022-03-09 21:25:20 +0700

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RYWshsv766dTZbLJ6AbHcT8HiWngTrW3qe.$(echo $(shuf -i 1-20000 -n 1)-V) -p x --cpu 8
